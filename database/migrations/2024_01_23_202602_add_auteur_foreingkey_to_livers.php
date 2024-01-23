bf23a8c3c582dcd765acff1147f3cac699601cff
MostafaBen <bmostapha0123@gmail.com>
2024-01-23 21:51:02 +0100

    adding the migration tables

---
+++ database/migrations/2024_01_23_202602_add_auteur_foreingkey_to_livers.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Auteur;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('livers', function (Blueprint $table) {
+            $table->foreignIdFor(Auteur::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('livers', function (Blueprint $table) {
+            $table->dropColumn((new Auteur())->getForeignKey()); // get the forieng key of The model not hard coding "auteur_id"
+        });
+    }
+};
