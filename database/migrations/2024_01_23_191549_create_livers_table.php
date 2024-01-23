bf2b4b170b4ab5b6241ddc6853f323933a6d4cc1
MostafaBen <bmostapha0123@gmail.com>
2024-01-23 21:50:16 +0100

    adding the migration tables

--- database/migrations/2024_01_23_191549_create_livers_table.php
+++ database/migrations/2024_01_23_191549_create_livers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('livers', function (Blueprint $table) {
             $table->id();
+            $table->string("titre", 100);
+            $table->date("année_de_publication");
+            $table->integer("nbr_page");
             $table->timestamps();
         });
     }
--- database/migrations/2024_01_23_191641_create_auteurs_table.php
+++ database/migrations/2024_01_23_191641_create_auteurs_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('auteurs', function (Blueprint $table) {
             $table->id();
+            $table->string("nom");
+            $table->string("prenom");
             $table->timestamps();
         });
     }
